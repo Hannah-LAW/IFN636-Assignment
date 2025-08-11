4a4145000de1e377310d26f656987cd5d9927f28
Hannah Law <hannah.lch.law@gmail.com>
2025-08-11 16:13:37 +1000

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,19 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
 
-dotenv.config();
+// config/db.js
+const mongoose = require("mongoose");
 
+// 令strictQuery行為與未來版本兼容
+mongoose.set('strictQuery', false);
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/tasks', require('./routes/taskRoutes'));
-
-// Export the app object for testing
-if (require.main === module) {
-    connectDB();
-    // If the file is run directly, start the server
-    const PORT = process.env.PORT || 5001;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const connectDB = async () => {
+  try {
+    console.log("Connecting to MongoDB URI:", process.env.MONGO_URI);  // 測試用
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
   }
+};
 
-
-module.exports = app
+module.exports = connectDB;
